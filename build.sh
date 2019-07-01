63ee741d86f221bfa8b16ac077eafd14f018fd90
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-07-01 00:36:40 -0500

    docker tag names cant be capitaliezd

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t communityHub .
+docker build -t communityhub .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dit --restart always --name communityHub -p 1997:80 communityHub
+docker run -dit --restart always --name communityhub -p 1997:80 communityhub
